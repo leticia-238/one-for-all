024b6f8494f20400f263ca3e034f38d03d88222f
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-06-10 10:54:20 -0300

    refatora desafio 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,23 +1,11 @@
-SELECT users.user_name as usuario, qtde_musicas_ouvidas, total_minutos
+SELECT 
+	users.user_name AS usuario, 
+  COUNT(history.user_id) AS qtde_musicas_ouvidas,
+  ROUND(SUM(songs.song_duration) / 60, 2) AS total_minutos
 FROM SpotifyClone.users AS users
-INNER JOIN
-	(	
-		SELECT user_id, COUNT(*) AS qtde_musicas_ouvidas
-		FROM SpotifyClone.history
-		GROUP BY user_id
-	) AS musicas_ouvidas 
-ON users.user_id = musicas_ouvidas.user_id
-INNER JOIN
-	(	
-		SELECT user_id, ROUND(SUM(song_duration) / 60, 2) as total_minutos
-		FROM
-			(
-				SELECT history.song_id, history.user_id, songs.song_duration
-				FROM SpotifyClone.history AS history
-				INNER JOIN SpotifyClone.songs AS songs 
-				ON history.song_id = songs.song_id
-			) AS songs_duration_by_user
-		GROUP BY user_id
-	) AS songs_duration
-ON users.user_id = songs_duration.user_id
+INNER JOIN SpotifyClone.history AS history 
+ON users.user_id = history.user_id
+INNER JOIN SpotifyClone.songs as songs
+ON songs.song_id = history.song_id
+GROUP BY usuario
 ORDER BY usuario ASC;
